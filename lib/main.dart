cf5bada5a810f991617ecbfeb8fb1053a01ce4fc
Elexandro Torres <elexandro.torres@gmail.com>
2022-03-17 19:40:06 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TelaCadastro());
+}
+
+class TelaCadastro extends StatelessWidget {
+  const TelaCadastro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
